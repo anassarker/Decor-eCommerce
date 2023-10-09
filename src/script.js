dc29c448397fd7048e9e0589ca03488671db2707
anassarker <anassarker500@gmail.com>
2023-10-10 01:23:40 +0600

    product page and menubar design upadated

--- src/script.js
+++ src/script.js
@@ -1,10 +1,30 @@
 const menu = document.querySelector("#menu");
 const menubar = document.querySelector("#menu-bar");
+const cartbtn = document.querySelector("#cart-button");
+
+const cartImage = document.querySelectorAll('.cart-image');
+const cartButton = document.querySelectorAll('#cart-button');
+
+
+cartImage.forEach((el,i) => {
+    cartImage[i].addEventListener('mouseenter', () => {
+        cartButton[i].style.transform = 'translateY(-25px)';
+    });
+    
+    cartImage[i].addEventListener('mouseleave', () => {
+        cartButton[i].style.transform = 'translateY(0)';
+    });
+});
+
+
+
+
 
 menu.addEventListener("click", () => {
     if (menubar.classList.contains("invisible")) {
         menubar.classList.remove("invisible");
-        menubar.classList.add("visible");
+        menubar.classList.add("visible", "slide-transition");
+        menubar.style.transform = 'translateX(0%)';
     }
 });
 
@@ -12,6 +32,7 @@ menubar.addEventListener("click", () => {
     if (menubar.classList.contains("visible")) {
         menubar.classList.remove("visible");
         menubar.classList.add("invisible");
+        menubar.style.transform = 'translateX(-100%)';
     }
 });
 
@@ -19,5 +40,8 @@ window.addEventListener("click", (event) => {
     if (event.target !== menu && event.target !== menubar) {
         menubar.classList.remove("visible");
         menubar.classList.add("invisible");
+        menubar.classList.remove("slide-transition");
+        menubar.style.transform = 'translateX(-100%)';
     }
 });
+
